51da2f89e4774f884fec93eaf550cb185f59c021
Felicia <felicia.calming@gmail.com>
2023-03-14 17:17:05 +0100

    (refactor): change files, imports from css to scss

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import "./style.css";
-import { createHtml } from "./src/js/createHtml";
-import toggleLightMode from "./src/js/toggleDarkmode";
+import './style.scss';
+import { createHtml } from './src/js/createHtml';
+import toggleLightMode from './src/js/toggleDarkmode';
 
 function init() {
   toggleLightMode();
