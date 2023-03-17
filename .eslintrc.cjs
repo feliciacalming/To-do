225b5a04f57f9c2a619cc9bbb5c527db297e3308
feliciacalming <felicia.calming@gmail.com>
2023-03-17 20:55:00 +0100

    (build): remove eslint-config-airbnb-base

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,16 +4,18 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'airbnb-base',
+    'standard-with-typescript',
     'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
   ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   rules: {
     'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
