197d07ffe12c77c312d94d44e8e76ed2bc40bb22
feliciacalming <felicia.calming@gmail.com>
2023-03-20 16:51:50 +0100

    (fix): handle ESLint errors in podcastService.ts

--- src/ts/services/podcastService.ts
+++ src/ts/services/podcastService.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosResponse } from 'axios';
-import { IPodcast } from '../models/IPodcast';
-import { ISRResponse } from '../models/ISRResponse';
+import axios from 'axios';
+import type { IPodcast } from '../models/IPodcast';
+import type { ISRResponse } from '../models/ISRResponse';
 
 export async function getPodcastsFromAPI(): Promise<IPodcast[]> {
   try {
-    const response: AxiosResponse<ISRResponse> = await axios.get<ISRResponse>(
+    const response = await axios.get<ISRResponse>(
       'https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false'
     );
     return response.data.programs;
