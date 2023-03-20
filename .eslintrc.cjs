9f12d3a4480e9cfd06983a50eb9dc8f4707c3c05
feliciacalming <felicia.calming@gmail.com>
2023-03-20 16:42:58 +0100

    (build): update configs in tsconfig, prettier and eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,24 +3,26 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'standard-with-typescript',
-    'prettier',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
   ],
-  overrides: [],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: true,
   },
-  rules: {
-    'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
+  rules: {},
 };
