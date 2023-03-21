54e71a54ba174bce3d810db9b943a34e2368b7f0
feliciacalming <felicia.calming@gmail.com>
2023-03-21 10:43:19 +0100

    (fix): handle ESLint errors in toggleDarkmode.ts
    closes #15

--- src/ts/toggleDarkmode.ts
+++ src/ts/toggleDarkmode.ts
@@ -1,17 +1,15 @@
-const toggleLightModeButton: HTMLButtonElement = document.querySelector(
-  '.toggle-btn'
-) as HTMLButtonElement;
+const toggleLightModeButton: HTMLButtonElement = document.querySelector('.toggle-btn') as HTMLButtonElement;
 toggleLightModeButton.addEventListener('click', toggleLightMode);
 console.log(toggleLightModeButton);
 
-export function toggleLightMode() {
+export function toggleLightMode(): void {
   document.body.classList.toggle('darkmode');
   if (document.body.classList.contains('darkmode')) {
     toggleLightModeButton.innerHTML = 'Välj mörkt läge';
     console.log('mörkt läge');
   } else {
     toggleLightModeButton.innerHTML = 'Välj ljust läge';
-    console.og('ljust läge');
+    console.log('ljust läge');
   }
 }
 
