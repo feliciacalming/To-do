05472833a21cb3fbe9ae48733186ca0278ee9e59
feliciacalming <felicia.calming@gmail.com>
2023-03-22 16:30:10 +0100

    (feat): added mock data

---
+++ src/ts/helpers/mockResponse.ts
@@ -0,0 +1,361 @@
+export const mockResponse = [
+  {
+    description: 'Norrländsk humor när den är som bäst. Sänds i P4 söndagar kl. 12.03-13.00.',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: 'Söndagar kl. 12.03-13.00',
+    email: '',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=2399',
+    programslug: 'mammasnyakille',
+    programimage:
+      'https://static-cdn.sr.se/images/2399/cd2152f2-8c81-4fb4-8585-dcc114594f20.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/2399/cd2152f2-8c81-4fb4-8585-dcc114594f20.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/2399/e134a81e-08b8-4ff5-955d-2df2515930df.png?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/2399/e134a81e-08b8-4ff5-955d-2df2515930df.png',
+    socialimage:
+      'https://static-cdn.sr.se/images/2399/cd2152f2-8c81-4fb4-8585-dcc114594f20.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/2399/cd2152f2-8c81-4fb4-8585-dcc114594f20.jpg',
+    socialmediaplatforms: [],
+    channel: {
+      id: 158,
+      name: '[No channel]',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Nils Eklund',
+    id: 2399,
+    name: 'Mammas Nya Kille',
+  },
+  {
+    description: 'Sylvass satir om nyheter, politik och en hel del kändisar.',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: 'Onsdagar kl 16 som podd - i P3-tablån onsdagar kl 23.07',
+    email: 'tankesmedjan@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=3718',
+    programslug: 'tankesmedjan',
+    programimage:
+      'https://static-cdn.sr.se/images/3718/ec4dc364-9278-4b2a-b8d1-59cb63c97478.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/3718/ec4dc364-9278-4b2a-b8d1-59cb63c97478.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/3718/bd6061f3-b0b5-4f59-8d4a-05169f6ce9c5.jpg?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/3718/bd6061f3-b0b5-4f59-8d4a-05169f6ce9c5.jpg',
+    socialimage:
+      'https://static-cdn.sr.se/images/3718/ec4dc364-9278-4b2a-b8d1-59cb63c97478.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/3718/ec4dc364-9278-4b2a-b8d1-59cb63c97478.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Twitter',
+        platformurl: 'https://twitter.com/tankesmedjanp3/',
+      },
+      {
+        platform: 'Instagram',
+        platformurl: 'https://instagram.com/tankesmedjanp3/',
+      },
+    ],
+    channel: {
+      id: 164,
+      name: 'P3',
+    },
+    archived: false,
+    hasondemand: true,
+    haspod: true,
+    responsibleeditor: 'Caroline Pouron',
+    id: 3718,
+    name: 'Tankesmedjan',
+  },
+  {
+    description:
+      'Lantzkampen är en tramsig frågesport om veckans nyheter, humor och allmänbildning i ett fint inslaget radiopaket. Programledare Annika Lantz. ',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: 'Fredagar 18.05 & lördagar 19.03 ',
+    email: 'lantzkampen@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=4469',
+    programslug: 'lantzkampen',
+    programimage:
+      'https://static-cdn.sr.se/images/4469/36562590-90e4-44c8-8593-f7f5b1b4df52.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/4469/36562590-90e4-44c8-8593-f7f5b1b4df52.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/4469/26e36915-f7ff-4267-b5a8-e8a93ab5b198.jpg?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/4469/26e36915-f7ff-4267-b5a8-e8a93ab5b198.jpg',
+    socialimage:
+      'https://static-cdn.sr.se/images/4469/36562590-90e4-44c8-8593-f7f5b1b4df52.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/4469/36562590-90e4-44c8-8593-f7f5b1b4df52.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Facebook',
+        platformurl: 'https://www.facebook.com/lantzkampenp1/',
+      },
+      {
+        platform: 'Instagram',
+        platformurl: 'https://instagram.com/lantzkampenp1/',
+      },
+    ],
+    channel: {
+      id: 132,
+      name: 'P1',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Katarina Dahlgren Svanevik',
+    id: 4469,
+    name: 'Lantzkampen',
+  },
+  {
+    description:
+      'I Fredag i P1 kavlar Kalle Lind och Susanna Dzamic upp ärmarna och vaskar fram guldklimparna ur veckans nyhetsflod. \n',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: '',
+    email: 'fredagip1@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=5054',
+    programslug: 'fredagip4',
+    programimage:
+      'https://static-cdn.sr.se/images/5054/76b90416-4234-43dd-b365-ac5efec6ba97.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/5054/76b90416-4234-43dd-b365-ac5efec6ba97.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/5054/20f94be4-b095-4163-a41a-1a2c5a21ee78.jpg?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/5054/20f94be4-b095-4163-a41a-1a2c5a21ee78.jpg',
+    socialimage:
+      'https://static-cdn.sr.se/images/5054/76b90416-4234-43dd-b365-ac5efec6ba97.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/5054/76b90416-4234-43dd-b365-ac5efec6ba97.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Facebook',
+        platformurl: 'https://www.facebook.com/FredagiP4',
+      },
+      {
+        platform: 'Twitter',
+        platformurl: 'https://twitter.com//',
+      },
+      {
+        platform: 'Instagram',
+        platformurl: 'https://instagram.com/fredagip4/',
+      },
+    ],
+    channel: {
+      id: 158,
+      name: '[No channel]',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Karin Arbsjö',
+    id: 5054,
+    name: 'Fredag i P1',
+  },
+  {
+    description: 'Ställ en fråga om precis vad som helst så gör vi så gott vi kan för att hitta svaret',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: 'Lördag 08.00',
+    email: 'safunkardet@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=5073',
+    programslug: 'safunkardet',
+    programimage:
+      'https://static-cdn.sr.se/images/5073/b1fc8711-b69d-4200-a11c-1cec7ab32cbf.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/5073/b1fc8711-b69d-4200-a11c-1cec7ab32cbf.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/5073/d6790369-f2b0-4751-bbd8-c531870e6e87.png?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/5073/d6790369-f2b0-4751-bbd8-c531870e6e87.png',
+    socialimage:
+      'https://static-cdn.sr.se/images/5073/b1fc8711-b69d-4200-a11c-1cec7ab32cbf.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/5073/b1fc8711-b69d-4200-a11c-1cec7ab32cbf.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Facebook',
+        platformurl: 'https://www.facebook.com/sverigesradiop1',
+      },
+      {
+        platform: 'Twitter',
+        platformurl: 'https://twitter.com/sverigesradioP1/',
+      },
+    ],
+    channel: {
+      id: 132,
+      name: 'P1',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Nina Glans',
+    id: 5073,
+    name: 'Så funkar det',
+  },
+  {
+    description:
+      'Satir med alternativa reportage från Utkantssverige, sänds i Godmorgon, världen! sommar, jul och påsk. Mycket nöje!',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    email: 'utkantssverige@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=5074',
+    programslug: 'utkantssverige',
+    programimage:
+      'https://static-cdn.sr.se/images/5074/0ad279be-fca8-41b7-b4be-729776c1e3e2.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/5074/0ad279be-fca8-41b7-b4be-729776c1e3e2.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/5074/d828ff1f-a6a2-4808-a78f-847c44f9115b.jpg?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/5074/d828ff1f-a6a2-4808-a78f-847c44f9115b.jpg',
+    socialimage:
+      'https://static-cdn.sr.se/images/5074/0ad279be-fca8-41b7-b4be-729776c1e3e2.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/5074/0ad279be-fca8-41b7-b4be-729776c1e3e2.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Facebook',
+        platformurl: 'https://www.facebook.com/dramasverigesradio/',
+      },
+      {
+        platform: 'Twitter',
+        platformurl: 'https://twitter.com//',
+      },
+      {
+        platform: 'Instagram',
+        platformurl: 'https://instagram.com/utkantssverige/',
+      },
+    ],
+    channel: {
+      id: 132,
+      name: 'P1',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Doreen Kanter',
+    id: 5074,
+    name: 'Utkantssverige',
+  },
+  {
+    description: 'Dockhumor med bland andra Birgitta, Kjell och Gloria Janman. ',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: 'Torsdag och fredag kl.10.50 ',
+    email: 'dockradio@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=5378',
+    programslug: 'dockradio',
+    programimage:
+      'https://static-cdn.sr.se/images/5378/007f8ec4-4e75-47b1-acf5-0fa77845821d.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/5378/007f8ec4-4e75-47b1-acf5-0fa77845821d.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/5378/4aca9e6d-ef80-4431-8d5b-1f65eacab47c.jpg?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/5378/4aca9e6d-ef80-4431-8d5b-1f65eacab47c.jpg',
+    socialimage:
+      'https://static-cdn.sr.se/images/5378/007f8ec4-4e75-47b1-acf5-0fa77845821d.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/5378/007f8ec4-4e75-47b1-acf5-0fa77845821d.jpg',
+    socialmediaplatforms: [],
+    channel: {
+      id: 158,
+      name: '[No channel]',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Doreen Kanter',
+    id: 5378,
+    name: 'Dockradio med Birgitta, Kjell, Gloria & Co',
+  },
+  {
+    description:
+      'Skruvad humorserie med Sissela Benn och Jonatan Unge om parterapi och svårigheten att ha en relation när man bestämt sig för att göra slut. ',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    payoff: 'Parterapi med Sissela Benn och Jonatan Unge',
+    broadcastinfo: 'Torsdag & fredag kl 10.50, säsongsvis.',
+    email: 'skarvor@sverigesradio.se',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=5383',
+    programslug: 'skarvoravettaktenskap',
+    programimage:
+      'https://static-cdn.sr.se/images/5383/8d631957-9e1f-4546-b825-01c92db9aade.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/5383/8d631957-9e1f-4546-b825-01c92db9aade.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/5383/aa24375b-418b-4f74-8f60-0669db685c20.png?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/5383/aa24375b-418b-4f74-8f60-0669db685c20.png',
+    socialimage:
+      'https://static-cdn.sr.se/images/5383/8d631957-9e1f-4546-b825-01c92db9aade.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/5383/8d631957-9e1f-4546-b825-01c92db9aade.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Facebook',
+        platformurl: 'https://sv-se.facebook.com/sverigesradioP4',
+      },
+    ],
+    channel: {
+      id: 158,
+      name: '[No channel]',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Doreen Kanter',
+    id: 5383,
+    name: 'Skärvor av ett äktenskap',
+  },
+  {
+    description:
+      'En non-true crime i 10 delar. En humorserie av och med William Spetz, Helena Lindegren och Sven Björklund.',
+    programcategory: {
+      id: 133,
+      name: 'Humor',
+    },
+    broadcastinfo: '24 september kl 7:00 släpps alla 10 delar ',
+    email: 'bokbussen@sr.se ',
+    phone: '',
+    programurl: 'https://sverigesradio.se/default.aspx?programid=5402',
+    programimage:
+      'https://static-cdn.sr.se/images/5402/e1151944-a94a-4138-9127-5505d0b8b257.jpg?preset=api-default-square',
+    programimagetemplate: 'https://static-cdn.sr.se/images/5402/e1151944-a94a-4138-9127-5505d0b8b257.jpg',
+    programimagewide:
+      'https://static-cdn.sr.se/images/5402/cbae1301-9ab1-4dc2-acaa-79a869861e0c.jpg?preset=api-default-rectangle',
+    programimagetemplatewide: 'https://static-cdn.sr.se/images/5402/cbae1301-9ab1-4dc2-acaa-79a869861e0c.jpg',
+    socialimage:
+      'https://static-cdn.sr.se/images/5402/e1151944-a94a-4138-9127-5505d0b8b257.jpg?preset=api-default-square',
+    socialimagetemplate: 'https://static-cdn.sr.se/images/5402/e1151944-a94a-4138-9127-5505d0b8b257.jpg',
+    socialmediaplatforms: [
+      {
+        platform: 'Facebook',
+        platformurl: 'https://www.facebook.com/sverigesradiop3',
+      },
+      {
+        platform: 'Instagram',
+        platformurl: 'https://instagram.com/p3sverigesradio/',
+      },
+    ],
+    channel: {
+      id: 164,
+      name: 'P3',
+    },
+    archived: false,
+    hasondemand: false,
+    haspod: true,
+    responsibleeditor: 'Matti Lilja ',
+    id: 5402,
+    name: 'Bokbussen i P3 ',
+  },
+];
